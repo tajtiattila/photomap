3ffd8491b404487b733de87015300eae5ae316f8
Attila Tajti <attila.tajti@gmail.com>
2016-05-06 10:55:03 +0200

    assign short url-escaped ids to images in Source

--- README.md
+++ README.md
@@ -7,5 +7,12 @@ If you have [Go](http://golang.org) photomap can be installed with:
 
     go get github.com/tajtiattila/photomap
 
-Then set the environment variable GOOGLEMAPS_APIKEY to your google maps api key,
+Then set the environment variable `GOOGLEMAPS_APIKEY` to your google maps api key,
 and start photomap with path(s) to your geotagged photos.
+
+TODO
+----
+
+- Group nearby photos together (avoid overlap)
+- Clickable photo icons
+- Add full screen photo view
--- cache.go
+++ cache.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha1"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"image"
@@ -10,6 +12,7 @@ import (
 	"log"
 	"path/filepath"
 	"sync"
+	"time"
 
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/tajtiattila/basedir"
@@ -19,9 +22,11 @@ type ImageCache struct {
 	src ImageSource
 	db  *leveldb.DB
 
-	m map[string]ImageInfo
+	// keysrcid and m are read-only after initialized in init()
+	keysrcid map[string]string
+	m        map[string]ImageInfo
 
-	pimtx sync.Mutex
+	pimtx sync.Mutex // protects pigen
 	pigen map[string]*genPhotoIcon
 }
 
@@ -35,10 +40,11 @@ func NewImageCache(src ImageSource) (*ImageCache, error) {
 		return nil, err
 	}
 	ic := &ImageCache{
-		src:   src,
-		db:    db,
-		m:     make(map[string]ImageInfo),
-		pigen: make(map[string]*genPhotoIcon),
+		src:      src,
+		db:       db,
+		keysrcid: make(map[string]string),
+		m:        make(map[string]ImageInfo),
+		pigen:    make(map[string]*genPhotoIcon),
 	}
 	return ic, ic.init()
 }
@@ -52,31 +58,31 @@ func (ic *ImageCache) Close() error {
 	return ic.db.Close()
 }
 
-func (ic *ImageCache) PhotoIcon(id string) (image.Image, error) {
+func (ic *ImageCache) PhotoIcon(key string) (image.Image, error) {
 	ic.pimtx.Lock()
-	pg, ok := ic.pigen[id]
+	pg, ok := ic.pigen[key]
 	if !ok {
 		pg = new(genPhotoIcon)
-		ic.pigen[id] = pg
+		ic.pigen[key] = pg
 	}
 	ic.pimtx.Unlock()
 
 	pg.once.Do(func() {
 		var err error
-		pg.im, err = ic.genPhotoIcon(pg, id)
+		pg.im, err = ic.genPhotoIcon(pg, key)
 		if err != nil {
 			log.Println(err)
 		}
 	})
 
 	if pg.im == nil {
-		return nil, fmt.Errorf("missing thumb for %s", id)
+		return nil, fmt.Errorf("missing thumb for %s", key)
 	}
 	return pg.im, nil
 }
 
-func (ic *ImageCache) genPhotoIcon(pg *genPhotoIcon, id string) (image.Image, error) {
-	k := []byte("photoicon|" + id)
+func (ic *ImageCache) genPhotoIcon(pg *genPhotoIcon, key string) (image.Image, error) {
+	k := []byte("photoicon|" + key)
 	data, err := ic.db.Get(k, nil)
 	switch err {
 	case nil:
@@ -89,10 +95,10 @@ func (ic *ImageCache) genPhotoIcon(pg *genPhotoIcon, id string) (image.Image, er
 		return nil, err
 	}
 
-	log.Println("thumbing", id)
+	log.Println("thumbing", ic.keysrcid[key])
 
 	// todo: sync.Once
-	rc, err := ic.src.Open(id)
+	rc, err := ic.src.Open(ic.keysrcid[key])
 	if err != nil {
 		return nil, err
 	}
@@ -123,53 +129,95 @@ func (ic *ImageCache) init() error {
 		return err
 	}
 
-	kbuf := append(make([]byte, 200), "imageinfo|"...)
-	vid := make([]string, 0, len(mmt))
-	for id, mt := range mmt {
-		data, err := ic.db.Get(append(kbuf, id...), nil)
-		if err == leveldb.ErrNotFound {
-			vid = append(vid, id)
-			continue
-		}
+	for srcid, mt := range mmt {
+		key, err := ic.getKey(srcid)
 		if err != nil {
 			return err
 		}
-		var ce cacheEntry
-		if err = json.Unmarshal(data, &ce); err != nil {
+		ic.keysrcid[key] = srcid
+		ce, err := ic.loadFreshCacheEntry(key, srcid, mt)
+		if err != nil {
 			return err
 		}
-		if ce.ModTime.Before(mt) {
-			vid = append(vid, id)
-			continue
+		if !ce.IsErr {
+			ic.m[key] = ce.ImageInfo
 		}
-		if ce.IsErr {
-			continue
-		}
-		ic.m[id] = ce.ImageInfo
 	}
 
-	var dbuf bytes.Buffer
-	for _, id := range vid {
-		ii, err := ic.src.Info(id)
-		var ce cacheEntry
-		if err != nil {
-			ce.IsErr = true
-		} else {
-			ce.ImageInfo = ii
-			ic.m[id] = ii
-		}
-		dbuf.Reset()
-		if err := json.NewEncoder(&dbuf).Encode(ii); err != nil {
-			return err
-		}
-		if err := ic.db.Put(append(kbuf, id...), dbuf.Bytes(), nil); err != nil {
-			return err
-		}
-	}
 	return nil
 }
 
+const imageInfoPfx = "imageinfo|"
+
+func (ic *ImageCache) loadFreshCacheEntry(key, srcid string, mt time.Time) (cacheEntry, error) {
+	k := []byte(imageInfoPfx + key)
+	data, err := ic.db.Get(k, nil)
+	if err == nil {
+		var ce cacheEntry
+		if err = json.Unmarshal(data, &ce); err != nil {
+			return cacheEntry{}, err
+		}
+		if ce.SrcId == srcid && ce.ModTime.Before(mt) {
+			// cache up to date
+			return ce, nil
+		}
+	}
+	ii, err := ic.src.Info(srcid)
+	ce := cacheEntry{SrcId: srcid}
+	if err != nil {
+		ce.IsErr = true
+	} else {
+		ce.ImageInfo = ii
+	}
+	data, err = json.Marshal(ce)
+	if err != nil {
+		panic("can't marshal cacheEntry")
+	}
+	return ce, ic.db.Put(k, data, nil)
+}
+
+func (ic *ImageCache) getKey(srcid string) (string, error) {
+	k := append([]byte("key|"), srcid...)
+	data, err := ic.db.Get(k, nil)
+	if err == nil {
+		return string(data), nil
+	}
+	if err != leveldb.ErrNotFound {
+		return "", err
+	}
+
+	hash := sha1.Sum([]byte(srcid))
+	h := hash[:9]
+	enc := base64.RawURLEncoding
+	prefix := []byte(imageInfoPfx)
+	key := make([]byte, len(prefix)+enc.EncodedLen(len(h)))
+	copy(key, prefix)
+	for {
+		enc.Encode(key[len(prefix):], h)
+		has, err := ic.db.Has(key, nil)
+		if err != nil {
+			return "", err
+		}
+		if !has {
+			// key not in use yet
+			err = ic.db.Put(k, key[len(prefix):], nil)
+			return string(key[len(prefix):]), err
+		}
+		incByteArray(h)
+	}
+}
+
+func incByteArray(p []byte) {
+	for i := len(p) - 1; i >= 0; i-- {
+		p[i]++
+		if p[i] != 0 {
+			return
+		}
+	}
+}
+
 type cacheEntry struct {
+	SrcId string
 	ImageInfo
 	IsErr bool
 }
